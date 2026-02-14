2bb3ab43058e60b7e331caebddb43cf047390ddb
Y-3LG <y3lgworld@gmail.com>
2026-02-13 20:58:34 -0400

    Refactor VerifyClient import for improved readability

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -4,9 +4,7 @@ export const dynamic = 'force-dynamic'
 
 import dynamicImport from 'next/dynamic'
 
-const VerifyClient = dynamicImport(() => import('./VerifyClient'), {
-  ssr: false,
-})
+const VerifyClient = dynamicImport(() => import('./VerifyClient'), { ssr: false })
 
 export default function Page() {
   return <VerifyClient />
